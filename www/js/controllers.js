2d5743dd3ba8b309e3361e378c799e64bd3efa54
John Paul Kee <johnpaulkee@gmail.com>
2015-10-23 19:04:12 -0700

    Added feedback button into more view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,36 +4,56 @@ angular.module('food-bank.controllers', [])
 // - `ionicMaterialInk.displayEffect()` (will need to happen once on controller activation and then repeat every time the objects update)
 // - `ionicMaterialMotion.ripple()` (etc.)
 
-.controller('NewsFeedCtrl', function($scope, $state, $location, $http, Newsposts) {
+.controller('NewsFeedCtrl', function($scope, $state, $location, $http, $ionicPopup, Newsposts, ionicMaterialInk) {
   $http.get("http://foodbank.herokuapp.com/newsposts.json")
-    .success(function(response) {
-     Newsposts.all(response);
-   }.bind(this));
+  .success(function(response) {
+   Newsposts.all(response);
+ }.bind(this));
 
   $scope.goToBlog = function() {
     $state.go('blog');
   };
+  ionicMaterialInk
 })
 
-.controller('MapCtrl', function($scope, $state, $location) {
+.controller('MapCtrl', function($scope, $state, $location, ionicMaterialInk) {
   $scope.goToBlog = function() {
     $state.go('blog');
   };
+  ionicMaterialInk.displayEffect();
 })
 
 .controller('MoreCtrl', function($scope, $state, $location, $ionicPopup, ionicMaterialInk) {
   $scope.goToBlog = function() {
     $state.go('blog');
   }
-  ionicMaterialInk.displayEffect();
   $scope.goToNotifications = function() {
     $state.go('notifications');
   }
+
+  var feedback_content = "Example Feedback content";
+
+  $scope.feedback = function() {
+    var alert = $ionicPopup.alert({
+      title: 'Feedback Form',
+      template: 'Here is some content that goes into the feedback form',
+      buttons: [
+      {
+        text:'Send Feedback', onTap: function(e) {
+          feedback_content = "The actual feedback content";
+          return true;
+        }
+      }
+      ]
+    })
+  }
+  ionicMaterialInk.displayEffect();
 })
 
 .controller('BlogCtrl', function($scope, $state, $location, $ionicPopup, ionicMaterialInk) {
+  ionicMaterialInk.displayEffect();
 })
 
 .controller('NotificationCtrl', function($scope, $state, $location, $ionicPopup, ionicMaterialInk) {
-
+  ionicMaterialInk.displayEffect();
 });
