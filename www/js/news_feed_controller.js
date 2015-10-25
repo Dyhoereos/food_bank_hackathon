fc9cf020dccfebb7bd20653d8e2f1c1af596d7f2
Maryna Zarud <mzarud@gmail.com>
2015-10-24 17:51:34 -0700

    Formatting notifications.

--- www/js/news_feed_controller.js
+++ www/js/news_feed_controller.js
@@ -4,18 +4,46 @@ app.controller('NewsFeedCtrl', function($scope, $state, $location, $http, NewsPo
 
     var validNewsPosts = [];
 
+    $scope.filterImportantPosts = function(postArray) {
+      if (postArray == undefined) {
+        return ""
+      }
+      return postArray.filter(function(post) {
+        if (post.tags == undefined) {
+          return false;
+        }
+        post.tags.forEach(function(tag) {
+          if (tag.name == "important") {
+            return true;
+          }
+        })
+        return false;
+      })
+    }
+
+    $scope.formatTags = function (tagsArray) {
+      if (tagsArray == undefined) {
+        return "";
+      }
+
+      var filteredString = "";
+      for (var i = 0; i < tagsArray.length; i++) {
+        filteredString += i.name + " ";
+      }
+      return filteredString;
+    }
+
     var refreshNewsPage = function (validNewsPosts) {
+
         return 0;
     };
 
     $http.get("http://foodbank.herokuapp.com/newsposts.json").then(function(response) {
         $scope.validNewsPosts = NewsPosts.all(response.data);
         refreshNewsPage(validNewsPosts);
-
     }.bind(this), function() {
         validNewsPosts = NewsPosts.fail();
         refreshNewsPage(validNewsPosts);
-
     }.bind(this));
 
     $scope.goToBlog = function() {
